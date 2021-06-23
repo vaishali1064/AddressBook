a21c0cd32780ea5d2c4cb8cd72e643c14c86ee7c
vaishali1064 <patelvaishali164@gmail.com>
2021-06-23 21:26:05 +0530

    Added: Welcome page

---
+++ AddressBook.java
@@ -0,0 +1,12 @@
+package addressBook;
+
+public class AddressBook 
+{	
+	
+		public static void main(String[] args) 
+		{
+			System.out.println("Welcome to Address Book Program");
+			
+		}	
+		
+}
